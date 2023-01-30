c0a1a42b30697fbd20f826d93ec76c8d31e06815
Xlaez <utibeabasiekong2@gmail.com>
2023-01-31 00:26:03 +0100

    feat: update server an add new important features

---
+++ internal/handlers/room.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	guuid "github.com/google/uuid"
+)
+
+func RoomCreate(c *fiber.Ctx) error {
+	return c.Redirect(fmt.Sprintf("/room/%s", guuid.New().String()))
+}
+
+func RoomWebSocket(c *fiber.Ctx) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+
+	_, _, room := createOrGetRoom(uuid)
+
+}
+
+func Room(c *fiber.Ctx) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		c.Status(400)
+		return nil
+	}
+
+	uuid, suuid, _ := createOrGetRoom(uuid)
+}
+
+func createOrGetRoom(uuid string) (string, string, Room) {
+
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,6 +3,15 @@ package server
 import (
 	"flag"
 	"os"
+	"time"
+
+	"go-videochat-conference/handlers"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html"
+	"github.com/gofiber/websocket/v2"
 )
 
 var (
@@ -17,4 +26,23 @@ func Run() error {
 	if *addr == ":" {
 		*addr = ":8081"
 	}
+
+	engine := html.New("../../views/", ".html")
+	app := fiber.New(fiber.Config{Views: engine})
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	app.Get("/", handlers.Welcome)
+	app.Get("/room/create", handlers.RoomCreate)
+	app.Get("/room/:uuid", handlers.Room)
+	app.Get("/room/:uuid/websocket", websocket.New(handlers.RoomWebSocket, websocket.Config{
+		HandshakeTimeout: 10 * time.Second,
+	}))
+	app.Get("/room/:uuid/chat", handlers.Room)
+	app.Get("/room/:uuid/chat/websocket", handlers.Room)
+	app.Get("/room/:uuid/viewer/websocket", handlers.Room)
+	app.Get("/stream/:suuid", handlers.Stream)
+	app.Get("/stream/:suuid/websocket", handlers.Room)
+	app.Get("/stream/:suuid/chat/websocket")
+	app.Get("/stream/:suuid/viewer/websocket")
 }
